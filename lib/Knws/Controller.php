8de288c128eb1408fa11ed0f1bd3b2e1ea9ba455
barif <w3db@yandex.ru>
2013-03-06 23:36:25 +0200

    Controller class

---
+++ lib/Knws/Controller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Description of Controller
+ * @see http://knws.ru/docs/Controller Documentation of Knws\Controller.
+ * @author Barif
+ */
+namespace Knws;
+
+class Controller
+{
+    /**
+     * run description
+     * @see http://knws.ru/docs/Controller/run Documentation of Knws\Controller->run().
+     * @return void
+     */
+    public static function run()
+    {
+        
+    }
+}
