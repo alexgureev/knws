68f24ad3d5c0afe0cbb43a25f34b8302ad290d44
barif <w3db@yandex.ru>
2013-03-09 23:37:10 +0200

    Application & Kernel classes

---
+++ lib/Knws/Application.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Description of Application
+ * @see http://knws.ru/docs/Application Documentation of Knws\Application.
+ * @author Barif
+ */
+
+namespace Knws;
+
+//use Knws\Class as Class;
+
+class Application
+{
+    
+}
---
+++ lib/Knws/Kernel.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Description of Kernel
+ * @see http://knws.ru/docs/Kernel Documentation of Knws\Kernel.
+ * @author Barif
+ */
+
+namespace Knws;
+
+//use Knws\Class as Class;
+
+class Kernel
+{
+    protected $bundles;
+
+    protected $bundleMap;
+    protected $container;
+    protected $rootDir;
+    protected $environment;
+    protected $debug;
+    protected $booted;
+    protected $name;
+    protected $startTime;
+    protected $classes;
+    protected $errorReportingLevel;
+
+    const VERSION         = '2.2.0';
+    const VERSION_ID      = '20100';
+    const MAJOR_VERSION   = '2';
+    const MINOR_VERSION   = '2';
+    const RELEASE_VERSION = '0';
+    const EXTRA_VERSION   = '';
+
+    /**
+     * Constructor.
+     *
+     * @param string  $environment The environment
+     * @param Boolean $debug       Whether to enable debugging or not
+     *
+     * @api
+     */
+    public function __construct($environment, $debug)
+    {
+        $this->environment = $environment;
+        $this->debug = (Boolean) $debug;
+        $this->booted = false;
+        $this->rootDir = $this->getRootDir();
+        $this->name = $this->getName();
+        $this->classes = array();
+        $this->bundles = array();
+
+        if ($this->debug) {
+            $this->startTime = microtime(true);
+        }
+
+        $this->init();
+    }
+
+    public function init()
+    {
+        ini_set('display_errors', 0);
+
+        if ($this->debug) {
+            error_reporting(-1);
+
+            DebugClassLoader::enable();
+            ErrorHandler::register($this->errorReportingLevel);
+            if ('cli' !== php_sapi_name()) {
+                ExceptionHandler::register();
+            } else {
+                ini_set('display_errors', 1);
+            }
+        }
+    }
+}
