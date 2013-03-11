ce7d85d4375dfc5e965dfde9bee2963837dca946
barif <w3db@yandex.ru>
2013-03-11 22:42:56 +0200

    Doctrine support

---
+++ lib/Knws/Service/Doctrine.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Description of Doctrine
+ * @see http://knws.ru/docs/Service/Doctrine Documentation of Knws/Service\Doctrine.
+ * @author Barif
+ */
+
+namespace Knws\Service;
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+class Doctrine extends \Knws\Service
+{
+    protected static $instance;
+    protected static $class;
+
+    /**
+     * Initialize EntityManager
+     * return void
+     */
+    public static function init()
+    {
+        $isDevMode = true;
+        //$config = Setup::createAnnotationMetadataConfiguration(array(APPPATH . "/entities"), $isDevMode);
+        $config = Setup::createYAMLMetadataConfiguration(array(APPPATH . "/configs/"), $isDevMode);
+        self::$instance = EntityManager::create(\Knws\Instance::$config['config']['Doctrine'], $config);
+    }
+}
